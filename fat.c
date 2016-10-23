99f2a9a1c3093fedc6ee7a35cbf2f14d7fc58db0
Matthew Dean <matt@Matthews-MacBook-Pro-4.local>
2016-10-23 00:00:10 -0400

    Shell implementation part 1

--- fat.c
+++ fat.c
@@ -30,6 +30,8 @@ FILE* FILE_SYSTEM_ID;
 int BYTES_PER_SECTOR = 512;
 
 bool startsWith(const char *pre, const char *str);
+int forkAndRunProcess(char *path, char **argv);
+char *splitInput(const char *argv, int *argc);
 
 extern int read_sector(int sector_number, char* buffer);
 extern int write_sector(int sector_number, char* buffer);
@@ -43,93 +45,43 @@ extern void set_fat_entry(int fat_entry_number, int value, char* fat);
 
 int main()
 {
-   unsigned char* boot;            // example buffer
+	unsigned char* boot;            // example buffer
 
-   int mostSignificantBits;
-   int leastSignificantBits;
-   int bytesPerSector;
+	int mostSignificantBits;
+	int leastSignificantBits;
+	int bytesPerSector;
 
-   // You must set two global variables for the disk access functions:
-   //      FILE_SYSTEM_ID         BYTES_PER_SECTOR
+	// You must set two global variables for the disk access functions:
+	//      FILE_SYSTEM_ID         BYTES_PER_SECTOR
 
-   // Use this for an image of a floppy drive
-   FILE_SYSTEM_ID = fopen("floppy1", "r+");
+	// Use this for an image of a floppy drive
+	FILE_SYSTEM_ID = fopen("floppy1", "r+");
 
-   if (FILE_SYSTEM_ID == NULL)
-   {
-      printf("Could not open the floppy drive or image.\n");
-      exit(1);
-   }
-
-   // Set it to this only to read the boot sector
-   BYTES_PER_SECTOR = BYTES_TO_READ_IN_BOOT_SECTOR;
-
-   // Then reset it per the value in the boot sector
-
-   boot = (unsigned char*) malloc(BYTES_PER_SECTOR * sizeof(unsigned char));
-
-   if (read_sector(0, boot) == -1)
-      printf("Something has gone wrong -- could not read the boot sector\n");
-
- 
-   // 12 (not 11) because little endian
-   mostSignificantBits  = ( ( (int) boot[12] ) << 8 ) & 0x0000ff00;
-   leastSignificantBits =   ( (int) boot[11] )        & 0x000000ff;
-   bytesPerSector = mostSignificantBits | leastSignificantBits;
-	
-	// Inspired by c-for-dummies.com/blog/?p=1112
-	char *buffer;
-	size_t bufsize = 200;
-	size_t characters;
-	const char delim[2] = " ";
-
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if(buffer == NULL)
+	if (FILE_SYSTEM_ID == NULL)
 	{
-		return 1;
+		printf("Could not open the floppy drive or image.\n");
+		exit(1);
 	}
 
-  	while(buffer[0] != '!')
-  	{	
-		characters = getline(&buffer, &bufsize, stdin);
-	
-		char *token;
-		
-		// Gets the number of arguments, delimited by space
-		// Inspired by SO question 13078926
-		int argc = 1;
-		char *ptr = buffer;
-		while((ptr = strchr(ptr, ' ')) != NULL) {
-			argc++;
-			ptr++;
-		}
-		
-		char **argv = malloc(argc * sizeof(char));
-		
-		token = strtok(buffer, delim);
+	// Set it to this only to read the boot sector
+	BYTES_PER_SECTOR = BYTES_TO_READ_IN_BOOT_SECTOR;
 
-		int i;
-		for(i = 0; i < argc; i++)
-		{
-			argv[i] = malloc(strlen(token) * sizeof(char));
-			argv[i] = token;
-			token = strtok(NULL, delim); 
-		}
+	// Then reset it per the value in the boot sector
 
-		if(startsWith("pbs", argv[0]) == 1)
-		{
-			printf("Print boot sector stuff\n");
-		}
-   	}   
+	boot = (unsigned char*) malloc(BYTES_PER_SECTOR * sizeof(unsigned char));
+
+	if (read_sector(0, boot) == -1)
+	{
+		printf("Something has gone wrong -- could not read the boot sector\n");
+	}
+
+	// 12 (not 11) because little endian
+	mostSignificantBits  = ( ( (int) boot[12] ) << 8 ) & 0x0000ff00;
+	leastSignificantBits =   ( (int) boot[11] )        & 0x000000ff;
+	bytesPerSector = mostSignificantBits | leastSignificantBits;
+
+	int code = runShell();	
 
 
-   return 0;
-}
-
-// From SO question 4770985
-bool startsWith(const char *pre, const char *str)
-{
-	size_t lenpre = strlen(pre),
-	       lenstr = strlen(str);
-	return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
+   	return 0;
 }
