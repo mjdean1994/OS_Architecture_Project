8cf91df0d847b250572b71951eabf9217a4bc6db
Matthew Dean <matt@Matthews-MacBook-Pro-4.local>
2016-11-02 20:25:49 -0400

    Support for no arguments with cd. Fixed bug with cding back to the root.

--- cd.c
+++ cd.c
@@ -7,11 +7,12 @@ FILE* FILE_SYSTEM_ID;
 
 int main(int argc, char **argv)
 {
-	if(argc != 2)
+	if(argc > 2)
 	{
 		printf("Wrong number of arguments! Usage: cd {path}\n");
 		exit(1);
 	}
+
 
 	//Inspired by https://beej.us/guide/bgipc/output/html/multipage/shm.html
  	int shmId = shmget((key_t) 8675308, sizeof(char), 0666 | IPC_CREAT);
@@ -28,8 +29,14 @@ int main(int argc, char **argv)
 
    	int flc; //first logical cluster
 
-   	
-	flc = new_searchForDirectory(argv[1], sharedMemory->firstLogicalCluster);
+   	if(argc == 1)
+   	{
+   		flc = 0;
+   	}
+   	else
+   	{
+   		flc = new_searchForDirectory(argv[1], sharedMemory->firstLogicalCluster);
+   	}
 	
 	if(flc == -1)
 	{
@@ -45,8 +52,16 @@ int main(int argc, char **argv)
 	printf("CDeez nutz\n");
 
 	char *newPath;
-	newPath = getCurrentDirectory(sharedMemory->currentDirectory, argv[1]); 
-
+	if(flc != 0)
+	{
+		newPath = getCurrentDirectory(sharedMemory->currentDirectory, argv[1]); 
+	}
+	else
+	{
+		newPath = "/";
+	}
+	
+	
 	strcpy(sharedMemory->currentDirectory, newPath);
  	sharedMemory->firstLogicalCluster = flc;
 	return 0;
